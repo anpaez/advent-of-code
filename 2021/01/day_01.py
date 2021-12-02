2bffb9e26e0938af129b9984bcce41099d36a13e
Andrés <andres.paez@deliveryhero.com>
2021-12-02 10:25:25 +0100

    add circular approach

--- 2021/01/day_01.py
+++ 2021/01/day_01.py
@@ -18,6 +18,22 @@ def count_increases(arr, window):
     return count
 
 
+def count_increase_circular_approach(arr, window):
+    count = 0
+    current_value = 0
+    queue = []
+    for i in range(len(arr) - window):
+        if len(queue) == window:
+            last_value = current_value
+            current_value -= queue.pop()
+            queue.append(arr[i])
+            current_value += arr[i]
+            if last_value < current_value:
+                count += 1
+
+
 array = read_file("input01.txt")
 print("Count of increase times is ", count_increases(array, 1))
+print("Count of increase times in circular approach is ", count_increases(array, 1))
 print("Count of increase in 3 number window", count_increases(array, 3))
+print("Count of increase in 3 number window in circular approach is ", count_increases(array, 3))
